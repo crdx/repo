12ab3d930eef6c87e9abb57cf8e972b631453fba
crdx <pub@crdx.org>
2021-07-30 21:21:46 +0100

    Reorder imports

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
-use crate::ignore::Ignorer;
 use std::env;
 use std::path::PathBuf;
+
+use crate::ignore::Ignorer;
 
 pub fn get_ignorer() -> Ignorer {
     let mut path = PathBuf::from(env::var_os("HOME").unwrap());
